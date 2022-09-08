1e77e96d00070ee5ddb206efcbe7f419c274236c
amaljom <amaljom99@gmail.com>
2022-09-08 18:16:16 +0200

    aggiunto il contenuto movie in altro elemento da con cui estendere home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Movie;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        $movies=Movie::all();
-        return view('home',compact("movies"));
+        return view('home');
     }
 }
