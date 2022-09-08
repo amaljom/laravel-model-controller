a8fcbf80fab4cafadbfb7c0300f63f2e758a1fa2
amaljom <amaljom99@gmail.com>
2022-09-08 17:51:57 +0200

    creazione header da includere

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class HomeController extends Controller
 {
     //
     public function index(){
-        $movies=movie::all();
-        return view('home');
+        $movies=Movie::all();
+        return view('home',compact("movies"));
     }
 }
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+
+<nav class="navbar">
+    <div>
+        <h1 class="title"> Pagina principale </h1>
+        <div>
+            <div class="title2"> Misualizza i movie sottostanti </div>
+        </div>
+    </div>
+</nav>
